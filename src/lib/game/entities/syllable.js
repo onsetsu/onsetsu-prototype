8383937cf2ec4b06a3b5dbadf8c4ebb0738792e0
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2014-12-25 21:03:31 +0100

    added Syllable functionality

--- src/lib/game/entities/syllable.js
+++ src/lib/game/entities/syllable.js
@@ -6,8 +6,7 @@ ig.module(
 )
 .defines(function(){
 
-// TODO: find and replace!!!
-Syllables = {
+SyllableDescriptions = {
 	'__usused1__': {
 		cost: -1,
 		label: '__usused1__',
@@ -17,168 +16,192 @@ Syllables = {
 	'__usused2__': {
 		cost: -1,
 		label: '__usused2__',
-		sheetIndex: 79
+		sheetIndex: 79,
 		primitives: ['__unused2__']
 	},
 	'Fire': {
 		cost: 2,
 		label: 'Fire',
-		sheetIndex: 80
+		sheetIndex: 80,
 		primitives: ['Fire']
 	},
 	'Water': {
 		cost: 2,
 		label: 'Water',
-		sheetIndex: 81
+		sheetIndex: 81,
 		primitives: ['Water']
 	},
 	'Earth': {
 		cost: 2,
 		label: 'Earth',
-		sheetIndex: 82
+		sheetIndex: 82,
 		primitives: ['Earth']
 	},
 	'Wind': {
 		cost: 2,
 		label: 'Wind',
-		sheetIndex: 83
+		sheetIndex: 83,
 		primitives: ['Wind']
 	},
 	'Light': {
 		cost: 2,
 		label: 'Light',
-		sheetIndex: 84
+		sheetIndex: 84,
 		primitives: ['Light']
 	},
 	'Shadow': {
 		cost: 2,
 		label: 'Shadow',
-		sheetIndex: 85
+		sheetIndex: 85,
 		primitives: ['Shadow']
 	},
 	'__usused3__': {
 		cost: -1,
 		label: '__usused3__',
-		sheetIndex: 86
+		sheetIndex: 86,
 		primitives: ['__usused3__']
 	},
 	'__usused4__': {
 		cost: -1,
 		label: '__usused4__',
-		sheetIndex: 87
+		sheetIndex: 87,
 		primitives: ['__usused4__']
 	},
 	'Kun': {
 		cost: 2,
 		label: 'Kun',
-		sheetIndex: 88
+		sheetIndex: 88,
 		primitives: ['Kun']
 	},
 	'Gam': {
 		cost: 5,
 		label: 'Gam',
-		sheetIndex: 89
+		sheetIndex: 89,
 		primitives: ['Gam']
 	},
 	'Nif': {
 		cost: 2,
 		label: 'Nif',
-		sheetIndex: 90
+		sheetIndex: 90,
 		primitives: ['Nif']
 	},
 	'Pai': {
 		cost: 1,
 		label: 'Pai',
-		sheetIndex: 91
+		sheetIndex: 91,
 		primitives: ['Pai']
 	},
 	'Chi': {
 		cost: 1,
 		label: 'Chi',
-		sheetIndex: 92
+		sheetIndex: 92,
 		primitives: ['Chi']
 	},
 	'Ma': {
 		cost: 1,
 		label: 'Ma',
-		sheetIndex: 93
+		sheetIndex: 93,
 		primitives: ['Ma']
 	},
 	'Ryo': {
 		cost: 2,
 		label: 'Ryo',
-		sheetIndex: 94
+		sheetIndex: 94,
 		primitives: ['Ryo']
 	},
 	'Ex': {
 		cost: 5,
 		label: 'Ex',
-		sheetIndex: 95
+		sheetIndex: 95,
 		primitives: ['Ex']
 	},
 	'Ren': {
 		cost: 3,
 		label: 'Ren',
-		sheetIndex: 96
+		sheetIndex: 96,
 		primitives: ['Ren']
 	},
 	'To': {
 		cost: 3,
 		label: 'To',
-		sheetIndex: 97
+		sheetIndex: 97,
 		primitives: ['To']
 	},
 	'Xau': {
 		cost: 5,
 		label: 'Xau',
-		sheetIndex: 98
+		sheetIndex: 98,
 		primitives: ['Xau']
 	},
 	'Yun': {
 		cost: 3,
 		label: 'Yun',
-		sheetIndex: 99
+		sheetIndex: 99,
 		primitives: ['Yun']
 	},
 	'FireDummy': {
 		cost: -1,
 		label: 'FireDummy',
-		sheetIndex: 100
+		sheetIndex: 100,
 		primitives: ['FireDummy']
 	},
 	'LightDummy': {
 		cost: -1,
 		label: 'LightDummy',
-		sheetIndex: 101
+		sheetIndex: 101,
 		primitives: ['LightDummy']
 	},
 	'WaterDummy': {
 		cost: -1,
 		label: 'WaterDummy',
-		sheetIndex: 102
+		sheetIndex: 102,
 		primitives: ['WaterDummy']
 	},
 	'EarthDummy': {
 		cost: -1,
 		label: 'EarthDummy',
-		sheetIndex: 103
+		sheetIndex: 103,
 		primitives: ['EarthDummy']
+	},
+	'Sol': {
+		cost: -1,
+		label: 'Sol',
+		sheetIndex: 106,
+		primitives: ['Sol', 'Light']
+	},
+	'Luna': {
+		cost: -1,
+		label: 'Luna',
+		sheetIndex: 107,
+		primitives: ['Luna', 'Shadow']
+	},
+	'Empty': {
+		cost: -1,
+		label: '',
+		sheetIndex: 108,
+		primitives: []
+	},
+	'Omnipotence': {
+		cost: -1,
+		label: 'Omnipotence',
+		sheetIndex: 109,
+		primitives: ['Omnipotence', 'Fire', 'Water', 'Earth', 'Wind', 'Light', 'Shadow']
 	},
 	'ShadowDummy': {
 		cost: -1,
 		label: 'ShadowDummy',
-		sheetIndex: 110
+		sheetIndex: 110,
 		primitives: ['ShadowDummy']
 	},
 	'WindDummy': {
 		cost: -1,
 		label: 'WindDummy',
-		sheetIndex: 111
+		sheetIndex: 111,
 		primitives: ['WindDummy']
 	},
 };
 
-var font = new ig.Font( 'media/onsetsufontblack.png' );
+var font = new ig.Font('media/onsetsufontblack.png');
 font.letterSpacing -= 2;
 font.lineSpacing -= 2;
 
@@ -188,6 +211,9 @@ EntitySyllable = EntityWidget.extend({
 	init: function(x, y, settings) {
 		this.parent(x, y, settings);
 
+        this.applySettings(settings);
+	},
+	applySettings: function(settings) {
         this.cost = settings.cost;
         this.label = settings.label;
         this.sheetIndex = settings.sheetIndex;
@@ -196,9 +222,6 @@ EntitySyllable = EntityWidget.extend({
 		this.addAnim(settings.label, 1, [settings.sheetIndex], true);
 		this.currentAnim = this.anims[settings.label];
 	},
-	update: function() {
-		this.parent();
-	},
 	draw: function() {
 		this.parent();
 		
@@ -206,7 +229,28 @@ EntitySyllable = EntityWidget.extend({
 		var x = this.pos.x + this.animSheet.width / 2;
 		var y = this.pos.y + this.animSheet.height / 4 * 3;
 		font.draw(this.label, x, y, ig.Font.ALIGN.CENTER);
+	},
+	matches: function(otherSyllable) {
+        return this.matchesPrimitives(otherSyllable.primitives);
+	},
+	matchesPrimitives: function(primitiveList) {
+        return _(primitiveList).difference(this.primitives).length === 0;
+	},
+	isEmpty: function() {
+	    return this.label == 'Empty' && this.primitives.length == 0;
+	},
+	copy: function() {
+	    return ig.game.spawnEntity(EntitySyllable, this.pos.x, this.pos.y, {
+            cost: this.cost,
+            label: this.label,
+            sheetIndex: this.sheetIndex,
+            primitives: this.primitives.slice()
+	    });
 	}
+});
+
+_.each(SyllableDescriptions, function(value, key) {
+    EntitySyllable['get' + key] = value;
 });
 
 });
